9ad2b7964d297cc7e847f8e49a3a5249fe4b5981
Jennifer Prince <jennifergraceprince@gmail.com>
2019-09-15 05:00:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,5 +1,5 @@
 def create_an_empty_array
-  create_an_empty_array = []
+  []
 end
 
 def create_an_array
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   things_i_am = ["hungry", "old", "tired", "sleepy"]
-  things_i_am.push("exhausted")
+  things_i_am << "exhausted"
 end
 
 def add_element_to_start_of_array(array, element)
